c2635f3b12fc343f834b0d0cb666e27aa1ea1622
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2025-12-10 19:09:23 +0100

    m

--- stencil/experiment.py
+++ stencil/experiment.py
@@ -3,22 +3,22 @@
 import os
 
 tests = [
-    ["./stencil --nb-iterations 10 --mesh-width 400 --mesh-height 400"],
-    ["./stencil --nb-iterations 10 --mesh-width 1000 --mesh-height 1000"],
-    ["./stencil --nb-iterations 10 --mesh-width 2000 --mesh-height 2000"],
-    ["./stencil --nb-iterations 10 --mesh-width 4000 --mesh-height 4000"],
-    ["./stencil_vec --nb-iterations 10 --mesh-width 400 --mesh-height 400"],
-    ["./stencil_vec --nb-iterations 10 --mesh-width 1000 --mesh-height 1000"],
-    ["./stencil_vec --nb-iterations 10 --mesh-width 2000 --mesh-height 2000"],
-    ["./stencil_vec --nb-iterations 10 --mesh-width 4000 --mesh-height 4000"],
-    ["./stencil_starpu --nb-iterations 10 --mesh-width 400 --mesh-height 400"],
-    ["./stencil_starpu --nb-iterations 10 --mesh-width 1000 --mesh-height 1000"],
-    ["./stencil_starpu --nb-iterations 10 --mesh-width 2000 --mesh-height 2000"],
-    ["./stencil_starpu --nb-iterations 10 --mesh-width 4000 --mesh-height 4000"],
-    ["./stencil_starpu_vec --nb-iterations 10 --mesh-width 400 --mesh-height 400"],
-    ["./stencil_starpu_vec --nb-iterations 10 --mesh-width 1000 --mesh-height 1000"],
-    ["./stencil_starpu_vec --nb-iterations 10 --mesh-width 2000 --mesh-height 2000"],
-    ["./stencil_starpu_vec --nb-iterations 10 --mesh-width 4000 --mesh-height 4000"],
+    "./stencil --nb-iterations 10 --mesh-width 400 --mesh-height 400",
+    "./stencil --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
+    "./stencil --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
+    "./stencil --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
+    "./stencil_vec --nb-iterations 10 --mesh-width 400 --mesh-height 400",
+    "./stencil_vec --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
+    "./stencil_vec --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
+    "./stencil_vec --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 400 --mesh-height 400",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
+    "./stencil_starpu_vec --nb-iterations 10 --mesh-width 400 --mesh-height 400",
+    "./stencil_starpu_vec --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
+    "./stencil_starpu_vec --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
+    "./stencil_starpu_vec --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
 ]
 
 for test in tests:
