c8446e73c916c0b5be4876a5e81abe458bd454f1
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2025-12-10 21:36:05 +0100

    exp3

--- stencil/experiment.py
+++ stencil/experiment.py
@@ -3,18 +3,14 @@
 import os
 
 tests = [
-    "./stencil --nb-iterations 10 --mesh-width 400 --mesh-height 400",
-    "./stencil --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
-    "./stencil --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
-    "./stencil --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
-    "./stencil_vec --nb-iterations 10 --mesh-width 400 --mesh-height 400",
-    "./stencil_vec --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
-    "./stencil_vec --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
-    "./stencil_vec --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
     "./stencil_starpu --nb-iterations 10 --mesh-width 400 --mesh-height 400",
     "./stencil_starpu --nb-iterations 10 --mesh-width 1000 --mesh-height 1000",
     "./stencil_starpu --nb-iterations 10 --mesh-width 2000 --mesh-height 2000",
     "./stencil_starpu --nb-iterations 10 --mesh-width 4000 --mesh-height 4000",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 200 --mesh-height 200",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 1500 --mesh-height 1500",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 3000 --mesh-height 3000",
+    "./stencil_starpu --nb-iterations 10 --mesh-width 3500 --mesh-height 3500",
 ]
 
 for test in tests:
