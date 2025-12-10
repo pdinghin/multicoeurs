4989886935269de2ecada88aed8c909e4be2f084
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2025-12-10 19:17:16 +0100

    m

--- stencil/stencil_vec.c
+++ stencil/stencil_vec.c
@@ -327,7 +327,7 @@ static void print_settings_csv(struct s_settings *p_settings)
 {
         FILE *fptr;
         fptr = fopen("vec.csv", "a+");
-        fprintf(fptr, "%d,%d,%d,%d,vec", p_settings->mesh_width, p_settings->mesh_height, p_settings->nb_iterations, p_settings->nb_repeat);
+        fprintf(fptr, "%d,%d,%d,%d,vec\n", p_settings->mesh_width, p_settings->mesh_height, p_settings->nb_iterations, p_settings->nb_repeat);
         fclose(fptr);
         //printf("%d,%d,%d,%d", p_settings->mesh_width, p_settings->mesh_height, p_settings->nb_iterations, p_settings->nb_repeat);
 }
